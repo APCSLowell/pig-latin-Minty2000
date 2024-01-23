85cefc1317dc803d771ce73f7c4787b4a50ef809
Minty2000 <142567186+Minty2000@users.noreply.github.com>
2024-01-23 12:07:36 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -38,6 +38,7 @@ public class PigLatin {
         return -1; 
     
     }
+    }
 
     public String pigLatin(String sWord) {
         //precondition: sWord is a valid String of length greater than 0
