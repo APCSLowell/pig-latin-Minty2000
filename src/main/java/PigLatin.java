c54b1327f0c7468d38c107e1f7c49eeefa282288
Minty2000 <142567186+Minty2000@users.noreply.github.com>
2024-01-23 12:12:01 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -35,9 +35,10 @@ public class PigLatin {
     		if(sWord.substring(i,i+1).equals("a")||sWord.substring(i,i+1).equals("e")||sWord.substring(i,i+1).equals("i")||sWord.substring(i,i+1).equals("o")||sWord.substring(i,i+1).equals("u")){
       return i;
     }
-        return -1; 
+       
     
     }
+	     return -1; 
     }
 
     public String pigLatin(String sWord) {
